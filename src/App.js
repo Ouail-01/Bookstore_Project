81197df52e3a86eb59fc747c763108524eb04472
Ouail-01 <ouaillaarabi@gmail.com>
2023-03-01 22:44:02 +0100

    Add the Provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+/* eslint-disable */
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from './components/navbar/Navbar';
 import Categories from './components/categories/Categories';
 import Books from './components/books/Books';
+import store from './redux/configureStore';
 
 const App = () => (
+  <>
+  <Provider store={store}>
   <Router>
     <Navbar />
     <Routes>
@@ -11,6 +16,8 @@ const App = () => (
       <Route path="/Categories" element={<Categories />} />
     </Routes>
   </Router>
+  </Provider>
+  </>
 );
 
 export default App;
