9fdddca01f0b108310aced0d6464f8f73f36dfe9
Ouail-01 <ouaillaarabi@gmail.com>
2023-03-02 10:22:01 +0100

    Update the redux file

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,27 +1,49 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADDBOOK = 'bookstore/books/ADDBOOK';
 const REMOVEBOOK = 'bookstore/books/REMOVEBOOK';
 
-const booksReduce = (state = [], action) => {
+const initialState = [
+  {
+    id: uuidv4(),
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+  },
+  {
+
+    id: uuidv4(),
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+  },
+  {
+
+    id: uuidv4(),
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+  },
+];
+
+const booksReduce = (state = initialState, action) => {
   switch (action.type) {
     case ADDBOOK:
-      return {
+      return [
         ...state,
-        books: [...state.title, action.type],
-      };
+        action.book,
+      ];
     case REMOVEBOOK:
-      return {
-        ...state,
-        books: state.title.filter((books) => books.id !== action.type),
-      };
+      return state.filter((book) => book.id !== action.book);
     default:
       return state;
   }
 };
 
-export const addBook = () => ({
+export const addBook = (book) => ({
   type: ADDBOOK,
+  book,
 });
-export const removeBook = () => ({
+
+export const removeBook = (book) => ({
   type: REMOVEBOOK,
+  book,
 });
 export default booksReduce;
