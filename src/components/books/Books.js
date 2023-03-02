ef0f0a68e04ffbb98b02841c2d9036f195a5e89a
Ouail-01 <ouaillaarabi@gmail.com>
2023-03-02 15:13:16 +0100

    Fetch the API

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,10 +1,18 @@
+/* eslint-disable */
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
+import { apiFetchBook } from '../../redux/books/booksSlice';
+import { useDispatch } from 'react-redux';
 
 const Books = () => {
   const booksList = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(apiFetchBook());
+  }, [dispatch]);
 
   return (
     <>
