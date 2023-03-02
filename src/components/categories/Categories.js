ebb2984aebe98ef7fce397b592027bafe7e2480f
Ouail-01 <ouaillaarabi@gmail.com>
2023-03-02 10:22:55 +0100

    Add the components

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux/es/exports';
+import { checkStatus } from '../../redux/categories/categoriesSlice';
 
-const Categories = () => (
-  <>
-    <button type="button">Check status</button>
-  </>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const handleStatus = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <>
+      <button type="button" onClick={handleStatus}>Check status</button>
+    </>
+  );
+};
+
 export default Categories;
