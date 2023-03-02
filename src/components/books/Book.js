0deb6099b9345944c4d6c90e3dfb2f9cf05e2679
Ouail-01 <ouaillaarabi@gmail.com>
2023-03-02 10:22:42 +0100

    Add the components

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { removeBook } from '../../redux/books/booksSlice';
+import { useDispatch } from 'react-redux';
 
-const Book = ({ title, author }) => (
-  <>
-    <h2>{title}</h2>
-    <p>{author}</p>
-  </>
-);
+const Book = ({ id, title, author }) => {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{author}</p>
+      <button type="button" onClick={handleClick}>Delete</button>
+    </>
+  );
+};
+
 Book.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
