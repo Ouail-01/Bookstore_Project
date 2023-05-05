053fbd06af72f82dea25abc56780d762b043c716
Ouail-01 <ouaillaarabi@gmail.com>
2023-05-05 19:30:08 +0100

    Add the Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 import Books from './components/books/Books';
 
 const App = () => (
   <Router>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Books />} />
     </Routes>
